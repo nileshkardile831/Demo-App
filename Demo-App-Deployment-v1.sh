bc0d10b39ef4df432d5cd82a92d31a8e82e7d8e4
root <root@ip-172-31-5-69.us-east-2.compute.internal>
2020-02-17 10:59:26 +0000

    added file to repo

---
+++ Demo-App-Deployment-v1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script is used to provision aws resources using terraform.
+
+set -xv
+
+home_dir=/var/lib/jenkins/workspace/Demo-App-Deployment/sample-app
+cd $home_dir
+
+# create service and deployment for bookinfo
+sudo /usr/bin/kubectl apply -f bookinfo/platform/kube/bookinfo.yaml
+
+# create virtual service, gateway and destination rule for bookinfo
+sudo /usr/bin/kubectl apply -f bookinfo/networking/virtual-service-all-v1.yaml
+sudo /usr/bin/kubectl apply -f bookinfo/networking/gateway-demoapp.yaml
+sudo /usr/bin/kubectl apply -f bookinfo/networking/destination-rule-demo-app.yaml
