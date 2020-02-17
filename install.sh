29c82c0126ab5be0fc07b4c7123d80b7a83839dd
sandeep2suri <60914874+sandeep2suri@users.noreply.github.com>
2020-02-17 18:31:47 +0530

    Update install.sh
    updated code to build and push docker images to docker registry

--- install.sh
+++ install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-# This script is used to provision aws resources using terraform.
+# This script is used to build, create and push docker images of our demo micro services.
 
 set -xv
 cd $home
+
+#build docker image from existing code
+./bookinfo/src/build-services.sh
+
+#push docker image to docker registry
+./bookinfo/build_push_update_images.sh
 
 # create service and deployment for bookinfo
 sudo /usr/bin/kubectl apply -f bookinfo/platform/kube/bookinfo.yaml
