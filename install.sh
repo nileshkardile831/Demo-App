79cc6eb9854270e838be79f5dae280224e3d6318
mrinmoymena <mrinmoymena@gmail.com>
2020-02-17 18:00:19 +0530

    Update install.sh
    Update work space path

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 # This script is used to provision aws resources using terraform.
 
 set -xv
-
-home_dir=/var/lib/jenkins/workspace/Demo-App-Deployment/sample-app
-cd $home_dir
+cd $home
 
 # create service and deployment for bookinfo
 sudo /usr/bin/kubectl apply -f bookinfo/platform/kube/bookinfo.yaml
